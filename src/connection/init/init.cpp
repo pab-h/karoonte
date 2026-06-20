da54d61c224d0c528e7abce9cbbc5d70f5b0b582
pab-h <dev.pab.2020@gmail.com>
2026-06-20 20:41:52 -0300

    feat: inicializando o mqtt

--- src/connection/init/init.cpp
+++ src/connection/init/init.cpp
@@ -4,14 +4,26 @@
 #include "init.hpp"
 
 #include "connection/tasks/tasks.hpp"
+#include "connection/mqtt/mqtt.hpp"
 
 namespace connection {
 
     void init() {
 
+        mqtt::init();
+
         xTaskCreate(
             tasks::keep_wifi_alive,
             "Keep wifi alive",
+            4096,
+            NULL,
+            1,
+            NULL
+        );
+
+        xTaskCreate(
+            tasks::keep_mqtt_alive,
+            "Keep mqtt alive",
             4096,
             NULL,
             1,
