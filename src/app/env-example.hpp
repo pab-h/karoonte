ee7f2327d49d0910f3a3f6cca76001a075fdbe04
pab-h <dev.pab.2020@gmail.com>
2026-06-20 20:42:01 -0300

    feat: Variáveis de conexão do mqtt

--- src/app/env-example.hpp
+++ src/app/env-example.hpp
@@ -3,3 +3,8 @@
 #define WIFI_SSID       "WiFi_Legal"
 #define WIFI_PASSWORD   "SenhaForte"
 #define WIFI_TIMEOUT_MS 5000
+
+#define MQTT_HOST         "broker.hivemq.com"
+#define MQTT_PORT         1883
+#define MQTT_TOPIC_PREFIX "karoonte"
+#define MQTT_CLIENT_ID    "aladdin"
