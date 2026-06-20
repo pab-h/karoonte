f321d63978789ad6c490a5b0d1d853d4edcc579b
pab-h <dev.pab.2020@gmail.com>
2026-06-20 20:58:01 -0300

    feat: Tópicos do sistema

--- src/app/env-example.hpp
+++ src/app/env-example.hpp
@@ -3,3 +3,12 @@
 #define WIFI_SSID       "WiFi_Legal"
 #define WIFI_PASSWORD   "SenhaForte"
 #define WIFI_TIMEOUT_MS 5000
+
+#define MQTT_HOST         "broker.hivemq.com"
+#define MQTT_PORT         1883
+#define MQTT_TOPIC_PREFIX "karoonte"
+#define MQTT_CLIENT_ID    "aladdin"
+
+#define MQTT_ANSWER_TOPIC MQTT_TOPIC_PREFIX "/" MQTT_CLIENT_ID "/" "answer"
+#define MQTT_RESULT_TOPIC MQTT_TOPIC_PREFIX "/" MQTT_CLIENT_ID "/" "result"
+#define MQTT_STATUS_TOPIC MQTT_TOPIC_PREFIX "/" "status"
