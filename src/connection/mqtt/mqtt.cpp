b5e97b951b6c5a881d78b56dbebd63b51638bfbe
pab-h <dev.pab.2020@gmail.com>
2026-06-20 21:19:14 -0300

    feat: Condição inicial para a primeira inscrição nos tópicos

--- src/connection/mqtt/mqtt.cpp
+++ src/connection/mqtt/mqtt.cpp
@@ -3,7 +3,10 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+#include "app/globals/events/events.hpp"
 #include "app/env.hpp"
+
+using namespace app::globals;
 
 namespace connection::mqtt {
 
@@ -22,6 +25,11 @@ namespace connection::mqtt {
 
         mqtt_client.setServer(MQTT_HOST, MQTT_PORT);
         mqtt_client.setCallback(topic_callback);
+
+        xEventGroupSetBits(
+            events::get_system_events(),
+            IS_MQTT_TOPIC_NOT_SUBSCRIBED_BIT
+        );
 
     }
 
